b987b426bb9af8d37853b6ed1124a95e19835878
akshaypandey28 <akshaychaman2004@gmail.com>
2026-04-25 13:20:58 +0530

    set up basic env

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const {PORT} = require('./config/serverConfig.js')
+const setUpAndStartServer = async () =>{
+    const app = express();
+
+    app.listen(PORT, () => {
+        console.log(`server started at ${PORT}`);
+    })
+}
+
+setUpAndStartServer();
