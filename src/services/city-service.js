bbbae015a9946dd142e33faf577967c43b935eac
akshaypandey28 <akshaychaman2004@gmail.com>
2026-04-30 00:29:22 +0530

    api router

--- src/services/city-service.js
+++ src/services/city-service.js
@@ -18,7 +18,7 @@ class CityService{
     async deleteCity(cityId){
         try {
             const response = await this.cityRepository.deleteCity(cityId);
-            return reponse;
+            return response;
         } catch (error) {
             console.log("Something went wrong at service layer");
             throw error;
